d09760c967b8c26676edf111221239b8f909f740
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2023-02-25 21:36:50 -0600

    Changed from numbers to images and added some functionaliuty to determine the # of adjacent bombs and the left/right click

--- vite-project/src/components/UI/Board/Board.tsx
+++ vite-project/src/components/UI/Board/Board.tsx
@@ -3,6 +3,7 @@ import Square from '../Square/Square'
 import "./Board.css"
 
 const Board = () => {
+  const [board, setBoard] = useState<JSX.Element[]>();
   var bombSquares: Array<number> = [];
 
   const selectBombSquares= () => {
@@ -16,27 +17,55 @@ const Board = () => {
   }
 
   const createBoard = () => {
+    // Change to effectRan only once in useEffect
+    console.log(bombSquares,"bmbsquares")
+  if(bombSquares.length !== 10) { 
     selectBombSquares();
 
     var squares: Array<JSX.Element> = [];
-    for (var i = 0; i <= 99; i++) {
-      if(bombSquares.indexOf(i) !== -1) {
-        squares.push(<Square key={i} type={"bomb"} />);
+    var adjacentSquares = [-11, -10, -9, -1, 1, 9, 10, 11]
+    var bombsInAdjacent = 0;
+    console.log(bombSquares);
+    for (var squareNum = 0; squareNum <= 99; squareNum++) {
+      if(bombSquares.indexOf(squareNum) !== -1) {
+        squares.push(<Square num={squareNum + 1} key={squareNum} type={"bomb"} />);
       } else {
-        squares.push(<Square key={i} type={"normal"} />);
+      // adjacentSquares = [ i-11, i-10, i-9, i-1, i+1, i+9, i+10, i+11 ];
+        for(var adjSquareNum = 0; adjSquareNum < adjacentSquares.length; adjSquareNum++) {
+          var currAdjNum = squareNum + adjacentSquares[adjSquareNum];
+          if(bombSquares.indexOf(currAdjNum) !== -1) {
+            console.log("currAdjNum", currAdjNum);
+            bombsInAdjacent++;
+          }
+        }
+        // -11, -10, -9, -1, 1, ,9, 10, 11
+        if(bombsInAdjacent > 0) {
+          squares.push(<Square num={squareNum + 1} key={squareNum} numberOfAdjacentBombs={bombsInAdjacent} type={"normal"} />);
+          bombsInAdjacent = 0;
+        } else {
+          squares.push(<Square num={squareNum + 1} key={squareNum} type={"normal"} />);
+        }
       }
     }
     return squares;
+ }
+    //setBoard(squares);
   }
 
+
   useEffect(() => {
+    
+    if(bombSquares.length != 10) { 
+      setBoard(createBoard());
+    }
     //selectBombSquares();
     //createBoard();
-  })
+  },[])
 
   return (
     <div className='board'>
-    { createBoard() }
+      {/* { createBoard() } */}
+      { board }
     </div>
   )
 }
--- vite-project/src/components/UI/Square/Square.tsx
+++ vite-project/src/components/UI/Square/Square.tsx
@@ -3,18 +3,24 @@ import "./Square.css"
 import Closed from "../../../assets/closed.png";
 
 type SquareProps = {
-  //  num: number
+    num: number
     type: string
+    numberOfAdjacentBombs?: number
 }
 
-const Square = ({type}: SquareProps) => {
+const Square = ({num, type, numberOfAdjacentBombs}: SquareProps) => {
 
-    const checkSquareType = () => {
-        
+
+    // Adjacent, if not bomb and has adjacens bomb: -11, -10, -9, -1, 1, 9, 10, 11
+    const checkSquareType = (e: any) => {
+       // e.preventDefault(); e.button where left click is 0, e.button where right click is 2
+       // console.log(e);
+       console.log("Type",type, "Num", num);
     }
-
   return (
-    <img className='square' onClick={checkSquareType} src={Closed} />
+    <section onMouseDown={checkSquareType} onContextMenu={(e) => {e.preventDefault()}}>
+        <img className='square' src={Closed} />
+    </section>
   )
 }
 
